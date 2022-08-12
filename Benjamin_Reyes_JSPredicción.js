cf256e04223ba77a8540496a2490c89b68a06b24
yggthzk <bgreyesq@gmail.com>
2022-08-12 12:49:08 -0400

    mi primer comit

---
+++ Benjamin_Reyes_JSPredicción.js
@@ -0,0 +1,10 @@
+
+    function myBirthYearFunc(){
+            console.log("Nací en " + 2006);
+        }
+   // se imprime "naci en 2006"    
+
+   function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+    // 

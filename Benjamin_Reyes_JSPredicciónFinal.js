821876fcdb1a1e15474a919883c92503d40a6830
yggthzk <bgreyesq@gmail.com>
2022-08-17 12:24:47 -0400

    subiendo preduccion.

---
+++ Benjamin_Reyes_JSPredicciónFinal.js
@@ -0,0 +1,19 @@
+
+    function myBirthYearFunc(){
+            console.log("Nací en " + 2006);
+        }
+   // se imprime "naci en 2006"    
+
+   function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+    // se imprime naci en el año segun el usuario
+    function add(num1, num2){
+            console.log("¡Sumando números!");
+            console.log("num1 is: " + num1);
+            console.log("num2 is: " + num2);
+            var sum = num1 + num2;
+            console.log(sum);
+        }
+        
+//        se imprime la soma de los numeros
